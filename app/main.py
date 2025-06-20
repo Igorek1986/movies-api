99ed845168bc1067426bc919a4f4dce9a4b2d293
Igor Ponomarev <9129910001@mail.ru>
2025-06-20 14:42:17 +0500

    Added CORS

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 import json
 import gzip
 import os
+from fastapi import Response
 
 app = FastAPI()
 
@@ -13,7 +14,17 @@ def load_data(category: str):
 
 
 @app.get("/{category}")
-async def get_movie(category: str, page: int = 1, per_page: int = 20):
+async def get_movie(
+    category: str,
+    page: int = 1,
+    per_page: int = 20,
+    response: Response = None,
+):
+
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+
     data = load_data(category)
     start = (page - 1) * per_page
     return data[start : start + per_page]
