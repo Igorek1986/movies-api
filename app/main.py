4444fb67907ea22387451ebc8b861c518e95b32d
Igor Ponomarev <9129910001@mail.ru>
2025-06-20 14:53:06 +0500

    Update endpoint function

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import json
 import gzip
 import os
 from fastapi import Response
+from math import ceil
 
 app = FastAPI()
 
@@ -20,11 +21,21 @@ async def get_movie(
     per_page: int = 20,
     response: Response = None,
 ):
-
+    # Устанавливаем CORS заголовки
     response.headers["Access-Control-Allow-Origin"] = "*"
     response.headers["Access-Control-Allow-Methods"] = "GET"
     response.headers["Access-Control-Allow-Headers"] = "Content-Type"
 
-    data = load_data(category)
+    all_data = load_data(category)
+    total_results = len(all_data)
+    total_pages = ceil(total_results / per_page)
+
     start = (page - 1) * per_page
-    return data[start : start + per_page]
+    results = all_data[start : start + per_page]
+
+    return {
+        "page": page,
+        "results": results,
+        "total_pages": total_pages,
+        "total_results": total_results,
+    }
